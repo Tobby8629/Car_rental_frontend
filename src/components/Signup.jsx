6beb5f1808017962ede32bde3ac7275aa6d1f7db
Samuel tobby <sarhmullah15@gmail.com>
2023-06-14 15:24:16 -0700

    registration of a new user

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './componentsCss/login.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { postUser } from './Redux/UserSlice';
 
 function Signup() {
+  const [user,setuser] = useState({username: "", email:""})
+  const dispatch = useDispatch()
+
+  const submit = (e) => {
+    e.preventDefault();
+    if(user.email === '' || user.username === '') {
+      return
+    }
+    dispatch(postUser({username: user.username}))
+  }
+
   return (
     <div className="form sign">
-      <form>
+      <form onSubmit={submit}>
         <h2>Sign up</h2>
-        <input type="text" id="firstname" placeholder="firstname" />
-        <input type="text" id="lastname" placeholder="lastname" />
-        <input type="text" id="username" placeholder="username" />
-        <input type="email" id="email" placeholder="email" />
+        <input type="text" id="username" value={user.username} placeholder="username" onChange={(e)=>setuser({...user, username:e.target.value})} />
+        <input type="email" id="email" value={user.email} placeholder="email" onChange={(e)=>setuser({...user, email: e.target.value})} />
         <div className="form-btn">
-          <button type="submit">Login</button>
+          <button type="submit">Sign up</button>
         </div>
         <p>
           already have an account?
