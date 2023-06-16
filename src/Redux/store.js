d5662a4265be070f3e96d6b296ad360a4d951180
Teklay <teklay@gmail.com>
2023-06-16 10:29:33 +0300

    Import car slicer

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import UserSlice from './UserSlice';
+import CarSlice from './CarSlice';
 
 const store = configureStore({
   reducer: {
     Users: UserSlice,
+    Cars: CarSlice,
   },
 });
 
