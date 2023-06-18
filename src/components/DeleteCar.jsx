e0777c5245b9eac882ae30bcdbf229126f62a79b
Teklay <teklay@gmail.com>
2023-06-18 20:07:40 +0300

    Fix linter bugs

--- src/components/DeleteCar.jsx
+++ src/components/DeleteCar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import './componentsCss/delete.css';
 import { useDispatch, useSelector } from 'react-redux';
