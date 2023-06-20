c6700fdf56b59368eefa69455cc3a8f78f5bccd6
Tekhlay <teklaybirhane12@yahoo.com>
2023-06-20 18:10:50 +0300

    Add test for navbar

---
+++ src/__tests__/Navabar.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Navbar from '../components/Navbar';
+import store from '../Redux/store';
+
+describe('Navbar Component Testing', () => {
+  test('Navebar Screenshot', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navbar />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders Navbar component text', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const linkElement = tree.getByText('CAR RENT');
+    expect(linkElement).toBeInTheDocument();
+  });
+});
